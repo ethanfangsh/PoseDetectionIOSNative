f9993d6edce239c2290e1ba25e90481c693aaba2
ethanfang <float4828@163.com>
2023-03-01 14:44:21 +0800

    test

---
+++ Classes/PodFrameExample.h
@@ -0,0 +1,18 @@
+//
+//  PodFrameExample.h
+//  PodFrameExample
+//
+//  Created by Spacecycle on 2023/3/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodFrameExample.
+FOUNDATION_EXPORT double PodFrameExampleVersionNumber;
+
+//! Project version string for PodFrameExample.
+FOUNDATION_EXPORT const unsigned char PodFrameExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodFrameExample/PublicHeader.h>
+
+int Test();
---
+++ Classes/PodFrameExample.m
@@ -0,0 +1,13 @@
+//
+//  PodFrameExample.m
+//  PodFrameExample
+//
+//  Created by Spacecycle on 2023/3/1.
+//
+
+#import <Foundation/Foundation.h>
+#import "PodFrameExample.h"
+
+int Test(){
+    return 0;
+}
